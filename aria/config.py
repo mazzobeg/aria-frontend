51c84f356df2ca32e20010142a3df4207df57255
mazzobeg <mazzobel.gvn@gmail.com>
2024-02-04 13:32:14 +0100

    refactor: :fire:

--- aria/config.py
+++
@@ -1,6 +0,0 @@
-"""
-This module contains the configuration for the SQLAlchemy database and the scrapers path.
-"""
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///aria.db"
-SCRAPERS_PATH = "/Users/giovannimazzobel/vscode-workspace/aria/scrapers"
